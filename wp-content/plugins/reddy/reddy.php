b0886bac434fc01d6e0cebe927cb223f69b5a7ab
LPMatrix <mubaraqsanusi908@gmail.com>
2020-12-29 17:07:50 +0100

    reddy plugin added

---
+++ wp-content/plugins/reddy/reddy.php
@@ -0,0 +1,12 @@
+<?php 
+	/**
+* Plugin Name: Reddy
+* Plugin URI: https://www.yourwebsiteurl.com/
+* Description: This is the very first plugin I ever created.
+* Version: 1.0
+* Author: Matrix
+* Author URI: http://yourwebsiteurl.com/
+**/
+
+wp_register_style( 'style', '/wp-content/plugins/reddy/css/reddy.css' );
+wp_enqueue_style('style');
